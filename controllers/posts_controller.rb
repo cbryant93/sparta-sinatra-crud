88f7669abffb6c048e7fd22684e16cb881264cb4
Tech-AYY <tech-ayy@Tech-AYYs-MacBook-Air.local>
2018-02-15 20:30:44 +0000

    git file structure setup

---
+++ controllers/posts_controller.rb
@@ -0,0 +1,20 @@
+class PostsController < Sinatra::Base
+
+  # sets root as the parent-directory of the current file
+  set :root, File.join(File.dirname(__FILE__), '..')
+
+  # sets the view directory correctly
+  set :views, Proc.new { File.join(root, "views") }
+
+  configure :development do
+      register Sinatra::Reloader
+  end
+
+
+get '/' do
+
+    erb :'posts/index'
+
+end
+
+end
